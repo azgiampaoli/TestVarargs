753dda7146d8adba974d04b89ca08af3d443d0a4
azgiampaoli <azgiampaoli@gmail.com>
2022-06-02 11:38:55 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        testVarargs ob = new testVarargs();
+
+        int total = ob.sum(5,2,3);
+        System.out.println("A soma é: " + total);
+
+
+
+    }
+}
